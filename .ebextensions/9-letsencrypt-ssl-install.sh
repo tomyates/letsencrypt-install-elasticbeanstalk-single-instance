cf8b28ee139be6e51af30f0fa099cd73c2c37242
Tom Yates <tom@tariffstreet.com>
2017-09-28 15:24:22 +0100

    added ebextensions files

---
+++ .ebextensions/9-letsencrypt-ssl-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Bash script to install lets encrypt SSL certificate as a POST HOOK
+# For use with Single instance PHP Elastic Beanstalk
+
+# Loadvars
+. /opt/elasticbeanstalk/support/envvars
+
+if [ "$LE_INSTALL_SSL_ON_DEPLOY" = true ] ; then
+
+    # Install mod_ssl
+    yum -y install mod24_ssl
+
+    # Install certbot
+    sudo mkdir /certbot
+    cd /certbot
+    wget https://dl.eff.org/certbot-auto;chmod a+x certbot-auto
+
+    # Create certificate
+    sudo ./certbot-auto certonly -d $LE_SSL_DOMAIN --agree-tos --email $LE_EMAIL --webroot --webroot-path /var/app/current/$DOCUMENT_ROOT --debug --non-interactive --renew-by-default
+
+    # Configure ssl.conf
+    sudo mv /etc/httpd/conf.d/ssl.conf.template /etc/httpd/conf.d/ssl.conf
+    sudo sed -i -e "s/{DOMAIN}/$LE_SSL_DOMAIN/g" /etc/httpd/conf.d/ssl.conf
+
+    # Install crontab
+    sudo crontab /tmp/cronjob
+
+    # Restart apache
+    sudo service httpd restart
+
+fi
